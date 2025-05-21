5cd84a4ff37d7e3db87523c24d2cffcfb3bb8a73
Adeyemi Rachael <adeyemitoluadedoyin@gmail.com>
2025-05-21 20:15:40 +0100

    feat: App middleware and Routes

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,16 @@
 import express from "express";
-import cors from "cors";
+import cors from "cors"; 
 
 const app = express();
+import paymentRoute from "./routes/payment";
 
 //Middlewares
 app.use(cors());
 app.use(express.json());
 
 
+//Routes
+app.use("/api/v1/", paymentRoute)
 
 export default app;
 
--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,7 @@ import app from "./app";
 
 const PORT = process.env.PORT ? parseInt(process.env.PORT, 10) : 4000;
 
-const start = async(): Promise<void> =>{
+const start = async() =>{
     try {
         app.listen(PORT, () =>{
             console.log(`Server is running on port ${PORT}`);
