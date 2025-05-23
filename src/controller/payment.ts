8929774da7f3b1cf901c903166eb6ed72de66269
Adeyemi Rachael <adeyemitoluadedoyin@gmail.com>
2025-05-23 07:08:48 +0100

    feat: stored user payment and added a get request to retrieve user payment information

--- src/controller/payment.ts
+++ src/controller/payment.ts
@@ -1,29 +1,35 @@
-import express, {Request, Response} from "express";
+import {Request, Response} from "express";
 import { StatusCodes } from "http-status-codes";
 import paystack from "../config/paystack";
-import  {paystackPostResponseData}  from "../utils/types";
+import  {payment, paystackPostResponseData, paystackVerifyResponseData}  from "../utils/types";
 
-export const transaction = async (req: Request, res: Response) =>{
+export const transaction = async (req: Request, res: Response) => {
     const {customer_name, customer_email, amount} = req.body;
 
     try {
         const initiatePayment = await paystack.post<paystackPostResponseData>("/transaction/initialize",{
             email: customer_email,
-            amount: amount * 100
-        }) as {
-             data: { 
-                data: {
-                    authorization_url: string,
-                    reference: string 
-                }
-            }
-        };
+            amount: amount * 100,
+            metadata: {customer_name}
+        });
+
+        const paymentInfo = initiatePayment.data.data;
+        const id = paymentInfo.reference;
+
+        payment.push({
+                id,
+                customer_name,
+                customer_email,
+                amount,
+                status: "pending"
+            });
+            
 
         res.status(StatusCodes.OK).json({
             status: true,
+            paymentInfo: payment,
             message: "Payment processing...",
-            payment_reference:initiatePayment.data.data.reference,
-            payment_URL: initiatePayment.data.data.authorization_url
+            payment_URL: paymentInfo.authorization_url
         });
     } 
     
@@ -33,4 +39,47 @@ export const transaction = async (req: Request, res: Response) =>{
             message:"Error processing payment"
         });
     }
+};
+
+
+export const displayTransaction = async (req: Request, res: Response) => {
+    const {id} = req.params;
+    const checkTransaction = payment.find(paymentID => paymentID.id == id);
+
+    try {
+        if (checkTransaction) {
+            const verify = await paystack.get<paystackVerifyResponseData>(`/transaction/verify/${id}`);
+            const paymentStatus = verify.data.data.status;
+            const paymentAmount = verify.data.data.amount;
+
+            checkTransaction.status = paymentStatus;
+            checkTransaction.amount = paymentAmount;
+
+           res.status(StatusCodes.OK).json({
+            payment: {
+              id: checkTransaction.id,
+              customer_name: checkTransaction.customer_name,
+              customer_email: checkTransaction.customer_email,
+              amount: checkTransaction.amount,
+              status: checkTransaction.status
+            },
+            status: "success",
+            message: "Payment details retrieved successfully."
+        });
+
+       } else {
+           res.status(StatusCodes.NOT_FOUND).json({
+               status: false,
+               message: "Payment detail not available."
+           });
+       }
+    } 
+
+    catch (error) {
+        res.status(StatusCodes.BAD_REQUEST).json({
+            status: false,
+            message:"Error retrieving payment",
+        });
+    }
+
 };
