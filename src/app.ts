6ee7ff83e5ffcaa3aa54df75a5f411a1cb572260
Adeyemi Rachael <adeyemitoluadedoyin@gmail.com>
2025-05-23 07:16:11 +0100

    feat: Added get request route

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,8 @@ import express from "express";
 import cors from "cors"; 
 
 const app = express();
-import paymentRoute from "./routes/payment";
+import payment from "./routes/payment";
+import payment_Details  from "./routes/payment";
 
 //Middlewares
 app.use(cors());
@@ -10,7 +11,8 @@ app.use(express.json());
 
 
 //Routes
-app.use("/api/v1/", paymentRoute)
+app.use("/api/v1/", payment);
+app.use("/api/v1/", payment_Details);
 
 export default app;
 
