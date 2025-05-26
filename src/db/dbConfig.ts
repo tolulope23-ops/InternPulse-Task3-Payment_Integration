3f06aef30460eb436bfb00159bf014406689a4f4
Adeyemi Rachael <adeyemitoluadedoyin@gmail.com>
2025-05-26 23:06:31 +0100

    feat: Establised mongoDB database connection

---
+++ src/db/dbConfig.ts
@@ -0,0 +1,20 @@
+// Loads environment variables from .env file
+import dotenv from 'dotenv';
+dotenv.config();
+import mongoose, { Mongoose } from 'mongoose';
+
+const MONGO_URI = process.env.dbURL; // MongoDB URI from environment variables
+
+const connectDB = async (): Promise<void> => {
+    try {
+        if (!MONGO_URI) {
+        throw new Error('MONGO_URI is not defined in environment variables');
+    }
+        await mongoose.connect(MONGO_URI);
+        console.log('Connected to Database');
+    } catch (error) {
+        console.log("Error connecting to Database:", error);
+    }
+};
+
+export default connectDB; // Exports connectDB function for use in other modules
