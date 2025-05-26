507910ed78d4cc6bebec360cd982574aa3de8f44
Adeyemi Rachael <adeyemitoluadedoyin@gmail.com>
2025-05-26 22:58:25 +0100

    refactor: Paystack API key from .env

--- src/config/paystack.ts
+++ src/config/paystack.ts
@@ -1,9 +1,13 @@
+import dotenv from "dotenv";    // Load environment variables from .env file
+dotenv.config();
 import axios from "axios";
 
+const PAYSTACK_SECRET_KEY = process.env.PAYSTACK_SECRET_KEY;  // Paystack secret key from environment variables
+ 
 const paystack = axios.create({
-    baseURL: "https://api.paystack.co",
+    baseURL: "https://api.paystack.co", // Base URL for Paystack API
     headers: {
-        "Authorization": `Bearer ${process.env.PAYSTACK_SECRET_KEY}`,
+        "Authorization": `Bearer ${PAYSTACK_SECRET_KEY}`, // Authorization header with Paystack secret key
         "content-Type": "application/json"
     }
 });
