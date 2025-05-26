db98266185516bf767dbe8ee8c7a5538cade7d82
Adeyemi Rachael <adeyemitoluadedoyin@gmail.com>
2025-05-26 23:02:35 +0100

    feat: Creates a database for payment

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,13 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import app from "./app";
+import connectDB from "./db/dbConfig";
 
 const PORT = process.env.PORT ? parseInt(process.env.PORT, 10) : 4000;
 
 const start = async() =>{
-    try {
+    try{
+        await connectDB();
         app.listen(PORT, () =>{
             console.log(`Server is running on port ${PORT}`);
         });
