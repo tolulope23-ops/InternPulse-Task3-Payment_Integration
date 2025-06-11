f670eb14150748312875706697a74c224a77b1e7
Adeyemi Rachael <adeyemitoluadedoyin@gmail.com>
2025-06-11 08:38:51 +0100

    feat: Sends app payment reference to paystack

--- src/services/paystack.services.ts
+++ src/services/paystack.services.ts
@@ -1,10 +1,11 @@
 import paystack from '../config/paystack';
 import { paystackPostResponseData, paystackVerifyResponseData } from '../utils/types';
 
-export const initiatePayment = async (email: string, amount: number) => {
+export const initiatePayment = async (email: string, amount: number, txn_reference: string) => {
     const paystackResponse = await paystack.post<paystackPostResponseData>('/transaction/initialize', {
     email,
-    amount
+    amount,
+    txn_reference
 });
     return paystackResponse.data.data;
 };
